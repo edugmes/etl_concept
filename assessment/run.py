ce649fcfd957837ca677e1e4e4790cd25d94d00c
Eduardo Gomes <edu.gmes0@gmail.com>
2022-09-04 20:49:25 -0300

    feat (datava handling): add class to download via url and store csv content to .csv and .json

--- assessment/run.py
+++ assessment/run.py
@@ -1,6 +1,17 @@
+import csv
+
+from csv_file_handler import CSVHandler
+
+
 def run():
     print("Hello, Profasee!")
 
 
 if __name__ == "__main__":
     run()
+    csv_handler = CSVHandler(
+        "https://profasee-data-engineer-assessment-api.onrender.com/people.csv"
+    )
+    csv_handler.download_files()
+    csv_handler.load_dataframe()
+    print(csv_handler.df.head())
