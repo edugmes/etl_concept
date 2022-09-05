b851ad8a07e49c58e382f196a9d8f16cd42f521c
Eduardo Gomes <edu.gmes0@gmail.com>
2022-09-05 03:30:01 -0300

    setup (database): change db config to use mysql

--- assessment/db_config.py
+++ assessment/db_config.py
@@ -2,5 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
-engine_params = "sqlite:///foo.db"
+# engine_params = "sqlite:///foo.db"
+user = "datatest"
+password = "alligator"
+root_pass = "root"
+port = 3306
+host = "database"
+database = "datatestdb"
+engine_params = f"mysql://{user}:{password}@{host}:{port}/{database}"
 engine = create_engine(engine_params, echo=False)
